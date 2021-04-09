426539c8b14c570146434768fae599d784c1a8b1
Lauren Overend <47351627+loverend@users.noreply.github.com>
2021-04-09 11:38:29 +0100

    Update Combine_extract_IMGT_information.py
    Hash extract,

--- Combine_extract_IMGT_information.py
+++ Combine_extract_IMGT_information.py
@@ -42,7 +42,7 @@ def Get_sample_files(batch_file):
 def Extract_sequences_for_IMGT(id,dir,batch_name):
   dir_use = dir[0]
   extract = True
-  extract = False
+  # extract = False
   if(extract):
     for batch in batch_name: 
       IMGT_file_compressed =dir_use+"ORIENTATED_SEQUENCES/ANNOTATIONS/IMGT_RAW/"+batch+".txz"
